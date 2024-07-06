9d1ed42bbeeae0c544db200657e1f42385fb4bc4
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-07-06 19:51:20 +0700

    Add files via upload

---
+++ Peremennyye.py
@@ -0,0 +1,9 @@
+# Создаем 4 переменные
+
+Number_of_completed_remote_sensing = 12 # Кол-во выввполненных ДЗ
+Number_of_hours_spent = 1.5 # Кол-во затраченных часов
+Course_name = 'Python' # Название курса
+Time_per_task = Number_of_hours_spent / Number_of_completed_remote_sensing # Вычисляем,используя 1 и 2 переменные
+
+# Вывод на экран
+print('Курс:', Course_name, 'всего задач:', Number_of_completed_remote_sensing,'затрачено часов:', Number_of_hours_spent, 'среднее время выполнения:', Time_per_task, 'часа.')
