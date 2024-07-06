0be4d89855ee56902a840eefbf194dcce72e885e
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-07-06 20:56:59 +0700

    Add files via upload

---
+++ Organizatsiya programm i metody strok.py
@@ -0,0 +1,10 @@
+# Создание переменной
+my_string = input('Введите текст: ')
+print(len(my_string)) # количество символов
+
+# Использование методов для строк
+print(my_string.upper()) # верхний регистр
+print(my_string.lower()) # нижний регистр
+print(my_string.replace(' ', '')) # удаляем все проблемы
+print(my_string[0]) # первый символ строки
+print(my_string[-1]) # последний символ строки
