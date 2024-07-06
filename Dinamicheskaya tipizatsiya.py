095c358295bac2a5f643727739250b75d4241183
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-07-06 20:08:03 +0700

    Add files via upload

---
+++ Dinamicheskaya tipizatsiya.py
@@ -0,0 +1,16 @@
+# Переменная с именем
+name = 'Vasiliy'
+print('Name:', name)
+
+# Переменная с возрастом
+age = 30
+print('Age:', age)
+
+# Перезапись в переменную с возврастом
+age = age + 1
+print('New Age:', age)
+
+# Переменная с логическим типов данных
+is_student = True
+print('Is Student:', is_student)
+
