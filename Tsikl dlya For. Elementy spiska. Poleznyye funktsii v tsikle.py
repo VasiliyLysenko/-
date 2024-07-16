e25badd90bf6ec465af53425d7b4dbbae6135d10
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-07-16 13:33:24 +0700

    Update Tsikl dlya For. Elementy spiska. Poleznyye funktsii v tsikle.py

--- Tsikl dlya For. Elementy spiska. Poleznyye funktsii v tsikle.py
+++ Tsikl dlya For. Elementy spiska. Poleznyye funktsii v tsikle.py
@@ -1,19 +1,23 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = [] # Список содержащий простые числа
 not_primes = [] # Список содержащий непростые числа
-is_prime = 0
+is_prime = True
+divider = 0
 
 for i in numbers:
+    if i == 1:
+        continue
     for j in range(1, i + 1):
         if i % j == 0:
-            is_prime += 1
-    if is_prime == 2:
+            divider += 1
+    if divider == 2:
+        is_prime = True
         primes.append(i)
-        is_prime = 0
+        divider = 0
     else:
+        is_prime = False
         not_primes.append(i)
-        is_prime = 0
+        divider = 0
 
 print(primes)
 print(not_primes)
-
