ec29e01d841c5dc086489731c9299485275b3e4c
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-08-04 11:05:36 +0700

    module_3_4.py

--- Proizvolnoye chislo parametrov.py
+++ Proizvolnoye chislo parametrov.py
@@ -2,12 +2,12 @@
 
 def single_root_words(root_word, *other_words):
     same_words = [] # Пустой список
-    root_word = root_word.lower() # По умолчанию делаем низкий регистр
+    root_word_lower = root_word.lower() # По умолчанию делаем низкий регистр
 
     for word in other_words: # Прописываем цикл
-        word = word.lower() # По умолчанию делаем низкий регистр
+        word_lower = word.lower() # По умолчанию делаем низкий регистр
 # Проверяем условие: если root_word(суффикс) попадается в каждом word или word в каждом root_word(суффикс). Добавляем в список.
-        if root_word in word or word in root_word:
+        if root_word_lower in word_lower or word_lower in root_word_lower:
             same_words.append(word)
 
     return same_words
