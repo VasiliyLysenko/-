8707f5059006c5525657bd1daaa57e39755767f0
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-08-25 01:10:57 +0700

    Update Introspektsiya.py

--- Introspektsiya.py
+++ Introspektsiya.py
@@ -3,8 +3,8 @@ def introspection_info(obj):
 
     obj_type = type(obj).__name__ # Тип объекта
 
-    attributes_ = [attr for attr in dir(obj)] # Находим атрибуты
-    methods_ = [method for method in dir(obj)] # Находим методы
+    attributes_ = [attr for attr in dir(obj)] if not callable(getattr(obj, attr)) and not attr.startswith("__")] # Находим атрибуты
+    methods_ = [method for method in dir(obj)] if callable(getattr(obj, method)) and not method.startswith("__")] # Находим методы
 
     module = inspect.getmodule(obj)
     module_name = module.__name__ if module is not None else '__main__' # Находим модуль
