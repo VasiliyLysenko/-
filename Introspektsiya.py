28dbdc4dd01880ef31f91536153bdd0ac5d465ca
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-08-25 01:21:09 +0700

    Update Introspektsiya.py

--- Introspektsiya.py
+++ Introspektsiya.py
@@ -1,22 +1,28 @@
 import inspect
 import sys
+
+
 def introspection_info(obj):
+    # Определяем тип объекта
+    obj_type = type(obj).__name__
 
-    obj_type = type(obj).__name__ # Тип объекта
+    # Получаем атрибуты и методы объекта
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    methods = [method for method in dir(obj)]
 
-    attributes_ = [attr for attr in dir(obj)] if not callable(getattr(obj, attr)) and not attr.startswith("__")] # Находим атрибуты
-    methods_ = [method for method in dir(obj)] if callable(getattr(obj, method)) and not method.startswith("__")] # Находим методы
-
+    # Получаем модуль, к которому принадлежит объект
     module = inspect.getmodule(obj)
-    module_name = module.__name__ if module is not None else '__main__' # Находим модуль
+    module_name = module.__name__ if module is not None else '__main__'
 
+    # Возвращаем собранную информацию в виде словаря
     return {
         'type': obj_type,
-        'attributes': attributes_,
-        'methods': methods_,
+        'attributes': attributes,
+        'methods': methods,
         'module': module_name
     }
 
+
+# Пример использования функции
 number_info = introspection_info(42)
 print(number_info)
-
