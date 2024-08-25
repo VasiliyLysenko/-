86706d21d6558a1c4d94904a1217b8fab2ffe615
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-08-25 13:19:27 +0700

    Add files via upload

---
+++ tests_12_1.py
@@ -0,0 +1,25 @@
+import runner
+import unittest
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner1 = runner.Runner('Test Runner1')
+        for _ in range(10):
+            runner1.walk()
+        self.assertEqual(runner1.distance, 50)
+
+    def test_run(self):
+        runner2 = runner.Runner('Test Runner2')
+        for _ in range(10):
+            runner2.run()
+        self.assertEqual(runner2.distance, 100)
+
+    def test_challenge(self):
+        runner3 = runner.Runner('Test Runner2')
+        for _ in range(10):
+            runner3.walk()
+        for _ in range(10):
+            runner3.run()
+        self.assertNotEqual(runner3.walk, runner3.run)
+
+if __name__ == '__main__':
+    unittest.main()
