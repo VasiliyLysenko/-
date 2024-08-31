6adb1353377238e173d3ce90504e0bdd4ced3126
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-08-31 17:30:56 +0700

    suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,8 +1,11 @@
 import runner as r1
 import runner2 as r2
 import unittest
+
+
 class RunnerTest(unittest.TestCase):
     is_frozen = False
+
     @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         runner1 = r1.Runner('Test Runner1')
@@ -24,6 +27,21 @@ class RunnerTest(unittest.TestCase):
             runner3.walk()
             runner3.run()
         self.assertNotEqual(runner3.walk, runner3.run)
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+    all_results = {}
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}  # Инициализация атрибута класса для хранения результатов всех тестов
+
+    def setUp(self):
+        self.runner1 = r2.Runner("Усэйн", speed=10)  # скорость 10
+        self.runner2 = r2.Runner("Андрей", speed=9)  # скорость 9
+        self.runner3 = r2.Runner("Ник", speed=3)  # скорость 3
+
 
 class TournamentTest(unittest.TestCase):
     is_frozen = True
@@ -63,7 +81,3 @@ class TournamentTest(unittest.TestCase):
         all_results = tournament.start()
         last_runner = max(all_results.keys())
         self.assertTrue(all_results[last_runner].name == "Ник")
-
-
-
-
