a7921dc55cbe7374b879e4cbd3775c036c70f059
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-09-24 20:17:08 +0700

    module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7257768067:AAErulgPP6DOkAqrvErIGlFun8hb04mWO-E"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
