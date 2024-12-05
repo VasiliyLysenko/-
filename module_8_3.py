13efb49e45d9df9863f3e9498ee65e9de79958fb
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-12-05 15:23:56 +0700

    Update module_8_3.py

--- module_8_3.py
+++ module_8_3.py
@@ -1,35 +1,32 @@
 class Car:
     def __init__(self, model, vin, numbers):
+        if self.__is_valid_vin(vin):
+            self.__vin = vin
+        if self.__is_valid_numbers(numbers):
+            self.__numbers = numbers
         self.model = model
-        self.__vin = vin
-        self.__numbers = numbers
-        self.__is_valid_vin()
-        self.__is_valid_numbers()
 
-    def __is_valid_numbers(self):
-        if not isinstance(self.__numbers, str):
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):
             raise IncorrectCarNumbers('Некорректный тип данных для номеров')
-        if len(self.__numbers) != 6:
+        if len(numbers) != 6:
             raise IncorrectCarNumbers('Неверная длина номера')
         return True
 
-    def __is_valid_vin(self):
-        if not isinstance(self.__vin, int):
+    def __is_valid_vin(self, vin):
+        if not isinstance(vin, int):
             raise IncorrectVinNumber('Некорректный тип vin номер')
-        if not 1000000 <= self.__vin <= 9999999:
+        if not 1000000 <= vin <= 9999999:
             raise IncorrectVinNumber('Неверный диапазон для vin номера')
         return True
-
 
 class IncorrectVinNumber(Exception):
     def __init__(self, message):
         self.message = message
 
-
 class IncorrectCarNumbers(Exception):
     def __init__(self, message):
         self.message = message
-
 
 try:
     first = Car('Model1', 1000000, 'f123dj')
