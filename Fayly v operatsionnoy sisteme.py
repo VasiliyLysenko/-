f8d53afbae90e070ea7feeada8ad26063c4b680b
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-12-05 15:19:36 +0700

    Fayly v operatsionnoy sisteme.py

--- Fayly v operatsionnoy sisteme.py
+++ Fayly v operatsionnoy sisteme.py
@@ -4,10 +4,10 @@ import time
 directory = '.'
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(directory, file)
-        filetime = os.path.getmtime(file)
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)
+        filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(filepath)
         print(f'Обнаружен файл: {file},  Путь: {filepath}, Размер: {filesize} байт,'
               f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
