2e89ef643a1916ddf12188d1d9a2a4975da859bf
VasiliyLysenko <162159798+VasiliyLysenko@users.noreply.github.com>
2024-12-10 15:26:03 +0700

    Домашнее задание по теме "Настраиваем СУБД postgre в django"

--- Module_19/module_19_6/last_project/blog_app/blog_app/settings.py
+++ Module_19/module_19_6/last_project/blog_app/blog_app/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'Datamases',
+'USER': 'postgres',
+'PASSWORD': '123456',
+'HOST': '127.0.0.1',
+'PORT': '5432',
+}
 }
 
 
